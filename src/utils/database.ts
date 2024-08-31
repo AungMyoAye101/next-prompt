5a4d375849a7617535d962b85e6319a37d0ce03a
AungMyoAye <aungmyoaye101@gmail.com>
2024-08-31 19:53:48 +0630

    connect db

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -11,7 +11,7 @@ export const connectToDb = async () => {
   } else {
     try {
       await mongoose.connect(process.env.MONGODB_URI!, {
-        dbName: "share_prompt",
+        dbName: "prompt",
         useNewUrlParser: true,
         useUnifiedTopology: true,
       } as ConnectOptions);
