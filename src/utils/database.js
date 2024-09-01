e31998afd9a36042236e28471367e60a2a8217d0
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-01 20:57:01 +0630

    update db

--- src/utils/database.js
+++ src/utils/database.js
@@ -23,7 +23,7 @@ export const connectToDb = async () => {
   // }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI!);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MONGODB");
   } catch (error) {
     console.log("Erro connecting to database: ", error);
