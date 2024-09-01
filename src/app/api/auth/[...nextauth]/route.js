8fe5148740afc5f8f2f5961fd13d8bc536f3d968
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-01 21:03:40 +0630

    update db

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -10,36 +10,36 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  callbacks: {
-    // async session({ session }) {
-    //   const sessionUser = await User.findOne({
-    //     email: session.user.email,
-    //   });
-    //   session.user.id = sessionUser._id.toString();
-    //   return session;
-    // },
-    async signIn({ account, profile, user }) {
-      try {
-        await connectToDb();
+  // callbacks: {
+  //   // async session({ session }) {
+  //   //   const sessionUser = await User.findOne({
+  //   //     email: session.user.email,
+  //   //   });
+  //   //   session.user.id = sessionUser._id.toString();
+  //   //   return session;
+  //   // },
+  //   async signIn({ account, profile, user }) {
+  //     try {
+  //       await connectToDb();
 
-        const userExists = await User.findOne({
-          email: profile?.email,
-        });
+  //       const userExists = await User.findOne({
+  //         email: profile?.email,
+  //       });
 
-        if (!userExists) {
-          await User.create({
-            email: profile?.email,
-            username: profile?.name?.replace(" ", "").toLocaleUpperCase(),
-            image: profile?.image,
-          });
-          return true;
-        }
-      } catch (error) {
-        console.log(error);
-        return false;
-      }
-    },
-  },
+  //       if (!userExists) {
+  //         await User.create({
+  //           email: profile?.email,
+  //           username: profile?.name?.replace(" ", "").toLocaleUpperCase(),
+  //           image: profile?.image,
+  //         });
+  //         return true;
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //       return false;
+  //     }
+  //   },
+  // },
 });
 
 export { handler as GET, handler as POST };
