bbed9b67b968b24ad2abbd20d12e66cc1762c78c
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-02 22:09:16 +0630

    add btn

--- src/components/PromptCreateForm.tsx
+++ src/components/PromptCreateForm.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const PromptCreateForm = () => {
@@ -30,6 +31,20 @@ const PromptCreateForm = () => {
             placeholder="tags your related field"
             className="p-2 border border-gray-200 rounded shadow-md "
           />
+        </div>
+        <div className="flex justify-end gap-2">
+          <Link
+            href="/"
+            className="px-4 py-2 text-sm text-mono border border-gray-200 rounded-md font-semibold shadow-md"
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="px-4 py-2 text-sm text-mono bg-orange-400 border border-gray-200 rounded-md font-semibold shadow-md"
+          >
+            Create
+          </button>
         </div>
       </form>
     </section>
