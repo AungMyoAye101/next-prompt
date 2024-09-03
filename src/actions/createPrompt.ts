2c22b4fca3c6af9ca1bac732991e94566dc38209
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-03 19:41:09 +0630

    add redirect and revalidatepath

--- src/actions/createPrompt.ts
+++ src/actions/createPrompt.ts
@@ -1,7 +1,13 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
 export const createPrompt = async (formData: FormData) => {
   const prompt = formData.get("prompt") as String;
   const tag = formData.get("tag") as String;
   console.log(prompt, tag);
+
+  redirect("/");
+  revalidatePath("/create");
 };
