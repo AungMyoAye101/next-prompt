9e24c588802272b7d9f0a5b7bee55ca9fa30c2d1
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-03 19:26:45 +0630

    add create prompt function

--- src/actions/createPrompt.ts
+++ src/actions/createPrompt.ts
@@ -1,1 +1,7 @@
 "use server";
+
+export const createPrompt = async (formData: FormData) => {
+  const prompt = formData.get("prompt") as String;
+  const tag = formData.get("tag") as String;
+  console.log(prompt, tag);
+};
