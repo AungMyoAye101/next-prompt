53a830da5bc3b471a01ffa8fda2d078bde0a5087
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-04 11:49:37 +0630

    add style to prompt card

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -3,8 +3,63 @@ import PromptCard from "./PromptCard";
 
 const Feed = () => {
   return (
-    <section>
-      <PromptCard />
+    <section className="grid grid-cols-4 gap-4 py-6">
+      <PromptCard
+        img=""
+        name="Zuely "
+        email="ama@gmail.com"
+        prompt="lorem i hff fbdf iii iii fdf  fdf"
+        tags="#webdev"
+      />
+      <PromptCard
+        img=""
+        name="Zuely "
+        email="ama@gmail.com"
+        prompt="lorem i hff fbdf iii iii fdf  fdf"
+        tags="#webdev"
+      />
+      <PromptCard
+        img=""
+        name="Zuely "
+        email="ama@gmail.com"
+        prompt="lorem i hff fbdf iii iii fdf  fdf"
+        tags="#webdev"
+      />
+      <PromptCard
+        img=""
+        name="Zuely "
+        email="ama@gmail.com"
+        prompt="lorem i hff fbdf iii iii fdf  fdf"
+        tags="#webdev"
+      />
+      <PromptCard
+        img=""
+        name="Zuely "
+        email="ama@gmail.com"
+        prompt="lorem i hff fbdf iii iii fdf  fdf"
+        tags="#webdev"
+      />
+      <PromptCard
+        img=""
+        name="Zuely "
+        email="ama@gmail.com"
+        prompt="lorem i hff fbdf iii iii fdf  fdf"
+        tags="#webdev"
+      />
+      <PromptCard
+        img=""
+        name="Zuely "
+        email="ama@gmail.com"
+        prompt="lorem i hff fbdf iii iii fdf  fdf"
+        tags="#webdev"
+      />
+      <PromptCard
+        img=""
+        name="Zuely "
+        email="ama@gmail.com"
+        prompt="lorem i hff fbdf iii iii fdf  fdf"
+        tags="#webdev"
+      />
     </section>
   );
 };
--- src/components/PromptCard.tsx
+++ src/components/PromptCard.tsx
@@ -1,22 +1,39 @@
 import Image from "next/image";
-import React from "react";
+import React, { FC } from "react";
 
-const PromptCard = () => {
+interface PromptCardProps {
+  img: string;
+  name: string;
+  email: string;
+  prompt: string;
+  tags: string;
+}
+
+const PromptCard: FC<PromptCardProps> = ({
+  img,
+  name,
+  email,
+  prompt,
+  tags,
+}) => {
   return (
-    <section>
-      <div>
-        <Image src={""} width={40} height={40} alt="user profile" />
+    <section className="flex flex-col gap-2 p-4 rounded-md border border-gray-500 shadow-lg ">
+      <div className="flex gap-2 items-center ">
+        <Image
+          src={img}
+          width={40}
+          height={40}
+          alt="user profile"
+          className="rounded-full "
+        />
         <div>
-          <h1>User name</h1>
-          <p>user@gmail.com</p>
+          <h1 className="text-md font-semibold font-serif ">{name}</h1>
+          <p className="text-sm text-gray-600 ">{email}</p>
         </div>
       </div>
       <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis iure
-          deserunt, rerum maxime beatae
-        </p>
-        <p>#webdev</p>
+        <p className="text-gray-700 font-serif ">{prompt}</p>
+        <p className=" font-serif text-sm font-semibold">{tags}</p>
       </div>
     </section>
   );
