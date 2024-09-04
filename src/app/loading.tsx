5f265fbf9ad40d6f0449f5dcdf8a76cc18db8dd1
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-04 10:45:47 +0630

    add loading ui

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="mt-10 flex items-center justify-center">
+      <img src="spinner.svg" alt="loading" />
+    </div>
+  );
+};
+
+export default loading;
