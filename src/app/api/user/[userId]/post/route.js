f3fafe0f474c4956f730b714678a2bd2a0357d12
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-05 22:30:15 +0630

    commit with user prompt error

---
+++ src/app/api/user/[userId]/post/route.js
@@ -0,0 +1,16 @@
+import Prompt from "@/models/prompt";
+
+import { connectToDb } from "@/utils/database";
+
+export const GET = async (req, { params }) => {
+  try {
+    await connectToDb();
+    const prompts = await Prompt.find({
+      author: ObjectId(params.userId),
+    }).populate("author");
+
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch prompt data", { status: 500 });
+  }
+};
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-
+import Prompt from "@/models/prompt";
 import Profile from "@/components/Profile";
 import PromptCard from "@/components/PromptCard";
+import { connectToDb } from "@/utils/database";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -9,20 +10,21 @@ import React, { useEffect, useState } from "react";
 
 const ProfilePage = () => {
   const { data: session } = useSession();
-  const [posts, setPosts] = useState({});
+  const [posts, setPosts] = useState([]);
 
-  const fetchPosts = async () => {
-    if (!session?.user) {
-      return;
-    }
-    const res = await fetch(`/api/users/${session?.user.id}/post`);
-    const data = await res.json();
-    setPosts(data);
-  };
   useEffect(() => {
-    fetchPosts();
-  }, []);
+    const fetchPosts = async () => {
+      if (!session?.user) {
+        return;
+      }
+      const res = await fetch(`/api/user/${session?.user.id}/post`);
+      const data = await res.json();
+      setPosts(data);
+    };
 
+    if (session?.user.id) fetchPosts();
+  }, []);
+  console.log(posts);
   return (
     <div>
       <Profile
@@ -31,29 +33,31 @@ const ProfilePage = () => {
         email={session?.user?.email!}
       />
       <section className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-4xl mx-auto gap-4 p-4">
-        <div className="flex flex-col gap-2 ">
-          <PromptCard
-            img={session?.user?.image!}
-            name={session?.user?.name!}
-            email={session?.user?.email!}
-            prompt="blabla"
-            tag="#ff"
-          />
-          <div className="flex justify-end gap-2">
-            <Link
-              href={"/"}
-              className="px-3 py-1 text-sm border bg-blue-400 shadow-md rounded-md text-slate-100"
-            >
-              Edit
-            </Link>
-            <Link
-              href={"/"}
-              className="px-3 py-1 text-sm border bg-red-400 shadow-md rounded-md text-slate-100"
-            >
-              Delete
-            </Link>
+        {posts.map((post) => (
+          <div className="flex flex-col gap-2 ">
+            <PromptCard
+              img={session?.user?.image!}
+              name={session?.user?.name!}
+              email={session?.user?.email!}
+              prompt={""}
+              tag={""}
+            />
+            <div className="flex justify-end gap-2">
+              <Link
+                href={"/"}
+                className="px-3 py-1 text-sm border bg-blue-400 shadow-md rounded-md text-slate-100"
+              >
+                Edit
+              </Link>
+              <Link
+                href={"/"}
+                className="px-3 py-1 text-sm border bg-red-400 shadow-md rounded-md text-slate-100"
+              >
+                Delete
+              </Link>
+            </div>
           </div>
-        </div>
+        ))}
       </section>
     </div>
   );
