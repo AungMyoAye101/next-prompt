11f1edc458333875ae80a29b01c34167c23fd0ed
AungMyoAye <aungmyoaye101@gmail.com>
2024-09-10 21:20:09 +0630

    update profile side nav

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -25,7 +25,7 @@ const Profile: FC<ProfileProp> = ({
   handelDelete,
 }) => {
   return (
-    <section className="flex flex-row  gap-4 mt-2 p-4 ">
+    <section className="flex flex-col  md:flex-row  gap-4 mt-2 p-4 ">
       <div className="flex flex-col items-center gap-3 p-4 border border-gray-300 shadow-md rounded-md ">
         <div className="relative size-24">
           <Image
@@ -35,31 +35,33 @@ const Profile: FC<ProfileProp> = ({
             className="object-cover rounded-full "
           />
         </div>
-        <div className="  flex flex-col justify-between gap-1 ">
+        <div className="  flex flex-col justify-between gap-2 ">
           <h1 className="text-md font-semibold text-gray-600 font-sans  w-full text-center p-1 transition-all ease-in-out duration-500 bg-orange-400 hover:shadow-md rounded hover:rounded-full">
             {name}
           </h1>
           <p className="text-sm text-gray-500 font-semibold font-serif rounded hover:rounded-full shadow border-gray-200 border w-full text-center p-2 transition-all ease-in-out duration-300 hover:bg-orange-400 hover:shadow-md">
             {email}
           </p>
-          <Link
-            href={"/create"}
-            className="text-sm text-gray-500 font-semibold font-serif rounded hover:rounded-full shadow border-gray-200 border w-full text-center p-2 transition-all ease-in-out duration-300 hover:bg-orange-400 hover:shadow-md"
-          >
-            create new
-          </Link>
-          <Link
-            href={"/"}
-            className="text-sm text-gray-500 font-semibold font-serif rounded hover:rounded-full shadow border-gray-200 border w-full text-center p-2 transition-all ease-in-out duration-300 hover:bg-orange-400 hover:shadow-md"
-          >
-            back to home
-          </Link>
-          <button
-            onClick={() => signOut()}
-            className="text-sm text-red-500 font-semibold font-serif rounded hover:rounded-full shadow border-gray-200 border w-full text-center p-2 transition-all ease-in-out duration-300 hover:text-gray-800  hover:bg-red-400 hover:shadow-md"
-          >
-            logout
-          </button>
+          <div className=" hidden  md:flex flex-col justify-between gap-2">
+            <Link
+              href={"/create"}
+              className="text-sm text-gray-500 font-semibold font-serif rounded hover:rounded-full shadow border-gray-200 border w-full text-center p-2 transition-all ease-in-out duration-300 hover:bg-orange-400 hover:shadow-md"
+            >
+              create new
+            </Link>
+            <Link
+              href={"/"}
+              className="text-sm text-gray-500 font-semibold font-serif rounded hover:rounded-full shadow border-gray-200 border w-full text-center p-2 transition-all ease-in-out duration-300 hover:bg-orange-400 hover:shadow-md"
+            >
+              back to home
+            </Link>
+            <button
+              onClick={() => signOut()}
+              className="text-sm text-red-500 font-semibold font-serif rounded hover:rounded-full shadow border-gray-200 border w-full text-center p-2 transition-all ease-in-out duration-300 hover:text-gray-800  hover:bg-red-400 hover:shadow-md"
+            >
+              logout
+            </button>
+          </div>
         </div>
       </div>
       <div>
